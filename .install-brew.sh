ec0654b88f0af540ce9f5f9f9f9cfa78d8e9aa86
afriesen6 <afriesen6@gatech.edu>
2015-05-15 01:30:46 -0400

    Install useful programs.

---
+++ .install-brew.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+brew update
+brew upgrade
+
+# Taps that I tend to use.
+brew tap homebrew/dupes
+brew tap homebrew/x11
+brew tap hombrew/science
+
+# Editors and other things I can't live without
+brew install git
+brew install vim --override-system-vi
+brew install bash-completion
+brew install tig --with-docs
+
+# Build systems I like
+brew install ant
+brew install gradle
+brew install sbt
+
+# Various languages I toy with
+brew install python
+brew install python3
+brew install clisp
+brew install elixir
+brew install erlang
+brew install gcc
+brew install lua
+brew install mit-scheme
+
+# Other useful utilities
+brew install antlr
+brew install fontforge
+brew install grep --with-default-names
+brew install rename
+brew install w3m
+brew install wget
---
+++ .install-cask.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+brew tap caskroom/cask
+brew install brew-cask
+
+# Utility Programs
+brew cask install adobe-reader
+brew cask install aquaterm
+brew cask install firefox
+brew cask install google-chrome
+
+# Productivity Helpers
+brew cask install audacity
+brew cask install dropbox
+brew cask install flux
+brew cask install gimp
+brew cask install skype
+brew cask install vlc
+
+# Games
+brew cask install battle-net
+brew cask install hex
+brew cask install openemu
+brew cask install steam
+
+brew tap homebrew/games
+brew install nethack4

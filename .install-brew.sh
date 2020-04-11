2929088a423591b8d52c24c09dcd449d0b615299
aaron <aaron@aaron-friesen.com>
2020-04-11 16:07:56 -0700

    OSX uses zsh now, so some of these need updating

--- .install-brew.sh
+++ .install-brew.sh
@@ -1,43 +1,17 @@
 #!/usr/bin/env bash
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew update
-brew upgrade
-
-# Taps that I tend to use.
-brew tap homebrew/dupes
-brew tap homebrew/x11
-brew tap hombrew/science
-
 # Editors and other things I can't live without
 brew install git
-brew install vim --override-system-vi
-brew install bash-completion
-brew install tig --with-docs
-
-# Build systems I like
-brew install ant
-brew install gradle
-brew install sbt
-brew install docker
-brew install boot2docker
+brew install vim
+brew install tig
 
 # Various languages I toy with
 brew install python
 brew install python3
-brew install clisp
-brew install elixir
-brew install erlang
 brew install gcc
-brew install lua
-brew install mit-scheme
-brew install typesafe-activator
 
 # Other useful utilities
-brew install antlr
-brew install fontforge
-brew install grep --with-default-names
+brew install grep
 brew install rename
 brew install w3m
 brew install wget
